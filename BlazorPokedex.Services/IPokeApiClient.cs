3d1dfaf8cf10406a025b55156c02af9bc7c4e207
MoraledaMoyL <MoraledaMoyL@G02ESXN00954>
2022-02-02 14:02:16 +0100

    Agregar archivos de proyecto.

---
+++ BlazorPokedex.Services/IPokeApiClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorPokedex.Models;
+
+namespace BlazorPokedex.Services
+{
+    public interface IPokeApiClient
+    {
+        Task<ResultObject> GetAllPokemons(PaginationParameters parameters);
+        Task<Pokemon> GetPokemon(string name);
+    }
+}
---
+++ BlazorPokedex.Services/PokeApiClient.cs
@@ -0,0 +1,31 @@
+﻿using BlazorPokedex.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPokedex.Services
+{
+    public class PokeApiClient : IPokeApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public PokeApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ResultObject> GetAllPokemons(PaginationParameters parameters)
+        {
+           return JsonConvert.DeserializeObject<ResultObject>(
+                await _httpClient.GetStringAsync($"pokemon?limit={parameters.PageSize}&offset={parameters.Offset}"));            
+        }
+
+        public async Task<Pokemon> GetPokemon(string name)
+        {
+            return JsonConvert.DeserializeObject<Pokemon>(
+                await _httpClient.GetStringAsync($"pokemon/{name}"));
+        }
+    }
+}
